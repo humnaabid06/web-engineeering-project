0b56eab4a8c14087e526bf7dcffb971b2d139814
humnaabid06 <153623490+humnaabid06@users.noreply.github.com>
2024-01-16 20:13:15 +0500

    Add files via upload

---
+++ GetStarted/GetStarted/Client/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+using System.Threading.Tasks;
+
+namespace GetStarted.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault();
+            builder.RootComponents.Add<App>("app");
+            await builder.Build().RunAsync();
+        }
+
+       
+    }
+}
---
+++ MvvmPattern/MvvmPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MvvmPattern
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault();
+            builder.RootComponents.Add<App>("app");
+            builder.Services.AddSingleton<MainModel>();
+            await builder.Build().RunAsync();
+        }
+    }
+}
